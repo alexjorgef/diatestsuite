52fa7e3f3f5aa48f8e695087df7b1b159e246038
Alexandre Ferreira <alexjorgef@protonmail.com>
2023-02-20 17:06:22 +0000

    build script
    Signed-off-by: Alexandre Ferreira <alexjorgef@protonmail.com>

---
+++ scripts/cluster-build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -e
+
+DIA_VM_PROFILE="diadata-vmprofile"
+
+__cluster_env_enable() {
+    echo "Enabling cluster environment ..."
+    eval "$(minikube -p "${1}" docker-env)"
+    if [ $? -eq 0 ]; then
+        echo "Cluster environment is now enabled"
+    else
+        echo "Cluster environment enabling failed"
+        exit 1
+    fi
+}
+__cluster_env_disable() {
+    echo "Disabling cluster environment ..."
+    eval "$(minikube docker-env -u)"
+    if [ $? -eq 0 ]; then
+        echo "Cluster environment is now disabled"
+    else
+        echo "Cluster environment disabling failed"
+        exit 1
+    fi
+}
+
+cd containers/db-influx
+
+if [ "$MINIKUBE_DRIVER" = "docker" ]; then
+    __cluster_env_disable
+fi
+
+make build
+
+if [ "$MINIKUBE_DRIVER" = "docker" ]; then
+    __cluster_env_enable "${DIA_VM_PROFILE}"
+fi
