3715ac6341e8300846d245bce379f8869be5f644
Alexandre Ferreira <alexjorgef@protonmail.com>
2023-05-17 18:31:37 +0100

    postgres entryscript edge

--- test/dump-3.sh
+++ test/dump-3.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-ls -la /postgres-dump/
-
-psql --host ${PGHOST} --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-query-asset.sql --output /tmp/dump-3-asset.csv
-psql --host ${PGHOST} --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-query-blockchain.sql --output /tmp/dump-3-blockchain.csv
-psql --host ${PGHOST} --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-query-exchange_cex.sql --output /tmp/dump-3-exchange_cex.csv
-psql --host ${PGHOST} --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-query-exchange_dex.sql --output /tmp/dump-3-exchange_dex.csv
-psql --host ${PGHOST} --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-query-exchangepair.sql --output /tmp/dump-3-exchangepair.csv
-psql --host ${PGHOST} --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-query-pool.sql --output /tmp/dump-3-pool.csv
+export PGHOST=${PGHOST_EXTRACT} PGUSER=${PGUSER_EXTRACT} PGDB=${PGDB_EXTRACT} PGPASSWORD=${PGPASSWORD_EXTRACT}; \
+psql --host ${PGHOST} --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-query-asset.sql --output /tmp/dump-3-asset.csv; \
+psql --host ${PGHOST} --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-query-blockchain.sql --output /tmp/dump-3-blockchain.csv; \
+psql --host ${PGHOST} --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-query-exchange_cex.sql --output /tmp/dump-3-exchange_cex.csv; \
+psql --host ${PGHOST} --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-query-exchange_dex.sql --output /tmp/dump-3-exchange_dex.csv; \
+psql --host ${PGHOST} --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-query-exchangepair.sql --output /tmp/dump-3-exchangepair.csv; \
+psql --host ${PGHOST} --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-query-pool.sql --output /tmp/dump-3-pool.csv; \
 psql --host ${PGHOST} --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-query-poolasset.sql --output /tmp/dump-3-poolasset.csv
-
-ls -la /tmp/
--- test/load-3.sh
+++ test/load-3.sh
@@ -1,10 +1,51 @@
 #!/usr/bin/env bash
 
-psql --host ${PGHOST_TEMP} --port 5432 --username ${PGUSER_TEMP} --dbname ${PGDB_TEMP} --file /postgres-dump/dump-3-load-blockchain.sql
-psql --host ${PGHOST_TEMP} --port 5432 --username ${PGUSER_TEMP} --dbname ${PGDB_TEMP} --file /postgres-dump/dump-3-load-exchange.sql
-psql --host ${PGHOST_TEMP} --port 5432 --username ${PGUSER_TEMP} --dbname ${PGDB_TEMP} --file /postgres-dump/dump-3-load-asset.sql
-psql --host ${PGHOST_TEMP} --port 5432 --username ${PGUSER_TEMP} --dbname ${PGDB_TEMP} --file /postgres-dump/dump-3-load-exchangepair.sql
-psql --host ${PGHOST_TEMP} --port 5432 --username ${PGUSER_TEMP} --dbname ${PGDB_TEMP} --file /postgres-dump/dump-3-load-poolasset.sql
-psql --host ${PGHOST_TEMP} --port 5432 --username ${PGUSER_TEMP} --dbname ${PGDB_TEMP} --file /postgres-dump/dump-3-load-pool.sql
+export PGHOST= PGUSER=${PGUSER_TEMP} PGDB=${PGDB_TEMP} PGPASSWORD=${PGPASSWORD_TEMP}
+psql --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-load-blockchain.sql
+psql --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-load-exchange.sql
+psql --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-load-asset.sql
+psql --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-load-pool.sql
+psql --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-load-poolasset.sql
+pg_dump -p 5432 -U $PGUSER --format c --blobs --verbose --dbname $PGDB --schema public --file /tmp/pgdump.sql
+echo TRY
+pg_dump -p 5432 -U $PGUSER --format c --blobs --verbose --dbname $PGDB --schema public --file /data/pv0001/pgdump.sql
+pg_dump -p 5432 -U $PGUSER --format c --blobs --verbose --dbname $PGDB --schema public --file /data/pv0003/pgdump.sql
+# id -u
+# id -g
+# id -u postgres
+# id -g postgres
+# ls -la /tmp/pgdump.sql
+# ls -la /data/pv0001/pgdump.sql
+# ls -la /data/pv0002
+# ls -la /data/pv0003
 
-tail -f /dev/null
+# psql --port 5432 --username ${PGUSER} --dbname ${PGDB} --file /postgres-dump/dump-3-load-exchangepair.sql; \
+# cat /var/lib/postgresql/data/postmaster.pid
+# pg_ctl stop -D /var/lib/postgresql/data -m smart
+# pg_ctl --help
+# pg_ctl stop -D /var/lib/postgresql/data -m smart
+# pg_ctl kill TERM 1
+
+# pg_ctl stop -m immediate -W -s
+
+# pg_ctl stop -D /var/lib/postgresql/data -W -s
+# pg_ctl -D "$PGDATA" stop
+# /usr/local/bin/pg_ctl stop -D /var/lib/postgresql/data -w -t 60 -m fast
+# kill -INT `head -1 /var/lib/postgresql/data/postmaster.pid`
+
+# su postgres -c "pg_ctl -D /var/lib/postgresql/data -l logfile stop"
+# pg_ctl status
+# ls -la /var/lib/postgresql/data/postmaster.pid
+# cat /var/lib/postgresql/data/postmaster.pid
+# pg_ctl -D /var/lib/postgresql/data -w -t 1000 stop
+head -1 /var/lib/postgresql/data/postmaster.pid
+# kill -TERM `head -1 /var/lib/postgresql/data/postmaster.pid`
+# pg_ctl -D "$PGDATA" -m immediate -w stop
+echo $?
+kill -TERM "$(head -1 /var/lib/postgresql/data/postmaster.pid)"
+# ls -la /var/lib/postgresql/data/postmaster.pid
+# cat /var/lib/postgresql/data/postmaster.pid
+# echo $?
+# pg_ctl status
+# exit 0
+# ls -la /var/lib/postgresql/data/postmaster.pid
