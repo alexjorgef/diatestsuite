25b9b60152227a6490f4e5864f63677319a5797d
Alexandre Ferreira <alexjorgef@protonmail.com>
2023-05-17 19:13:14 +0100

    clean-5

--- dumper/scripts/minikubeInstallSnap.sh
+++ dumper/scripts/minikubeInstallSnap.sh
@@ -5,5 +5,4 @@ kubectl create secret docker-registry regcred \
     --docker-username="alex1a" \
     --docker-password="R3Uf&nq@A9&hv&" \
     --docker-email="alex.jorge.m@gmail.com"
-# kubectl create -f "deployments/k8s-yaml/postgres-cron-pod.yaml"
-kubectl create -f "deployments/k8s-yaml/postgres-cron-pod-extract.yaml"
+kubectl create -f "deployments/k8s-yaml/postgres-snapshot-cron.yaml"
