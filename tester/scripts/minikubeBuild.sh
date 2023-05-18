5c303c977cdac655eff38cdd098bb2d726169553
Alexandre Ferreira <alexjorgef@protonmail.com>
2023-05-18 01:59:06 +0100

    prepare the test

--- tester/scripts/minikubeBuild.sh
+++ tester/scripts/minikubeBuild.sh
@@ -8,12 +8,9 @@ eval "$(minikube -p $minikube_profile docker-env)"
 
 . ./baseimage.sh --notpush
 
-docker build -f "build/Dockerfile-assetCollectionService" -t "${image_tag}.assetcollectionservice:latest" .
-docker build -f "build/Dockerfile-blockchainservice" -t "${image_tag}.blockchainservice:latest" .
 docker build -f "build/Dockerfile-filtersBlockService" -t "${image_tag}.filtersblockservice:latest" .
 docker build -f "build/Dockerfile-genericCollector" -t "${image_tag}.exchangescrapercollector:latest" .
 docker build -f "build/Dockerfile-liquidityScraper" -t "${image_tag}.liquidityscraper:latest" .
-docker build -f "build/Dockerfile-pairDiscoveryService" -t "${image_tag}.pairdiscoveryservice:latest" .
 docker build -f "build/Dockerfile-tradesBlockService" -t "${image_tag}.tradesblockservice:latest" .
 
 eval "$(minikube -p $minikube_profile docker-env --unset)"
