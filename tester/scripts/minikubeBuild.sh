037198742892188f7e243d0b1821827a6f8c6b3a
Alexandre Ferreira <alexjorgef@protonmail.com>
2023-05-22 14:37:40 +0100

    include pairdisocvery service in order to populate cache

--- tester/scripts/minikubeBuild.sh
+++ tester/scripts/minikubeBuild.sh
@@ -11,6 +11,7 @@ eval "$(minikube -p $minikube_profile docker-env)"
 docker build -f "build/Dockerfile-filtersBlockService" -t "${image_tag}.filtersblockservice:latest" .
 docker build -f "build/Dockerfile-genericCollector" -t "${image_tag}.exchangescrapercollector:latest" .
 docker build -f "build/Dockerfile-liquidityScraper" -t "${image_tag}.liquidityscraper:latest" .
+docker build -f "build/Dockerfile-pairDiscoveryService" -t "${image_tag}.pairdiscoveryservice:latest" .
 docker build -f "build/Dockerfile-tradesBlockService" -t "${image_tag}.tradesblockservice:latest" .
 
 eval "$(minikube -p $minikube_profile docker-env --unset)"
