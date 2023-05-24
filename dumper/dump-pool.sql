9b3ea30144fb91ae30e7d40c8196641e30b2ac06
Alexandre Ferreira <alexjorgef@protonmail.com>
2023-05-24 16:27:35 +0100

    change snapshot from 48 hours to 7 days

--- dumper/dump-pool.sql
+++ dumper/dump-pool.sql
@@ -1,5 +1,5 @@
 COPY (
     SELECT * FROM pool WHERE pool_id IN (
-        SELECT DISTINCT pool_id FROM poolasset WHERE "time_stamp" >= now() - interval '48 hours'
+        SELECT DISTINCT pool_id FROM poolasset WHERE "time_stamp" >= now() - interval '7 days'
     )
 ) TO STDOUT WITH (format csv, delimiter ';');
--- dumper/dump-poolasset.sql
+++ dumper/dump-poolasset.sql
@@ -1,3 +1,3 @@
 COPY (
-    SELECT * FROM poolasset WHERE "time_stamp" >= now() - interval '48 hours'
+    SELECT * FROM poolasset WHERE "time_stamp" >= now() - interval '7 days'
 ) TO STDOUT WITH (format csv, delimiter ';');
