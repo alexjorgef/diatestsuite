45c24b604ac77be4e57ba293716d4e8ac85dad7a
Alexandre Ferreira <alexjorgef@protonmail.com>
2023-05-24 16:34:29 +0100

    clean trash files

--- .temp/scripts/minikubeStartScrapers.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl create -f "deployments/k8s-yaml/exchangescraper-bitfinex.yaml"
-kubectl create -f "deployments/k8s-yaml/exchangescraper-bittrex.yaml"
-kubectl create -f "deployments/k8s-yaml/exchangescraper-coinbase.yaml"
-kubectl create -f "deployments/k8s-yaml/exchangescraper-mexc.yaml"
--- .temp/scripts/minikubeStopScrapers.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl delete -f "deployments/k8s-yaml/exchangescraper-bitfinex.yaml"
-kubectl delete -f "deployments/k8s-yaml/exchangescraper-bittrex.yaml"
-kubectl delete -f "deployments/k8s-yaml/exchangescraper-coinbase.yaml"
-kubectl delete -f "deployments/k8s-yaml/exchangescraper-mexc.yaml"
